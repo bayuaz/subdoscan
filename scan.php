7247fbca8ae919bc64cd587821fb1d3d377eb3b1
khaganzz <rindonesiabayu@gmail.com>
2021-05-28 02:59:41 +0700

    Mass Scan

--- scan.php
+++ scan.php
@@ -1,42 +1,107 @@
 <?php
 
-$green = "\e[32m";
-$red = "\033[0;31m";
-$clear = "\e[0m";
+error_reporting(0);
 
-echo " $green
-   ____     __      __                _    
-  / __/_ __/ /  ___/ /__  __ _  ___ _(_)__ 
- _\ \/ // / _ \/ _  / _ \/  ' \/ _ `/ / _ \
-/___/\_,_/_.__/\_,_/\___/_/_/_/\_,_/_/_//_/
-  / __/______ ____  ___  ___ ____          
- _\ \/ __/ _ `/ _ \/ _ \/ -_) __/          
-/___/\__/\_,_/_//_/_//_/\__/_/             
+function banner() {
+	include 'warna.php';
 
-   $red Coded by Khatulistiwa $clear
-";
+	echo $hijau;
+	echo "   ____     __      __                _\n";
+	echo "  / __/_ __/ /  ___/ /__  __ _  ___ _(_)__\n";
+	echo " _\ \/ // / _ \/ _  / _ \/  ' \/ _ `/ / _ \ \n";
+	echo "/___/\_,_/_.__/\_,_/\___/_/_/_/\_,_/_/_//_/\n";
+	echo "  / __/______ ____  ___  ___ ____\n";
+	echo " _\ \/ __/ _ `/ _ \/ _ \/ -_) __/\n";         
+	echo "/___/\__/\_,_/_//_/_//_/\__/_/\n\n";
 
-echo "\nURL (ex: site.com): ";
-$input = trim(fgets(STDIN));
+	echo $merah;
+	echo "    Coded by Khatulistiwa";
+	echo $clear;     
 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, "https://sonar.omnisint.io/subdomains/".$input);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-$output = curl_exec($ch); 
-curl_close($ch); 
-
-$hasil = json_decode($output, true);
-echo "\n";
-
-if ($hasil == null) {
-	echo $red . "Wrong input or site doesn't have subdomain! $clear \n";
-} else {
-	foreach($hasil as $key => $list) {
-	    echo $key+1 . ". " . $list . "\n";
-	    $fp = fopen('result.txt', 'a+');
-		fwrite($fp, $list."\n");
-		fclose($fp);
-	}
-
-	echo "$green \nSaved in result.txt! $clear \n";
+	menu();
 }
+
+function menu() {
+	include 'warna.php';
+
+	echo "\n\n{$cyan}1. Single List";
+	echo "\n2. Multi List";
+	echo "\n\n{$kuning}Masukkan pilihan: ";
+	$pilih = trim(fgets(STDIN));
+
+	if ($pilih == 1) {
+		singleList();
+	} else if ($pilih == 2) {
+		multipleList();
+	} else {
+		echo "\n{$merah}Pilihan anda tidak valid!{$clear}\n";
+	}
+}
+
+function singleList() {
+	include 'warna.php';
+
+	echo "URL (ex: site.com): ";
+	$input = trim(fgets(STDIN));
+
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, "https://sonar.omnisint.io/subdomains/$input");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	$output = curl_exec($ch); 
+	curl_close($ch);
+
+	$hasil = json_decode($output, true);
+	echo "\n$clear";
+
+	if (is_null($hasil)) {
+		echo "{$merah}Wrong input or site doesn't have subdomain!{$clear}\n";
+	} else {
+		foreach ($hasil as $no => $list) {
+		    echo $no+1 . ". $list\n";
+		    $fp = fopen("./result/single/$input.txt", 'a+');
+			fwrite($fp, "$list\n");
+			fclose($fp);
+		}
+
+		echo "\n{$cyan}Saved in ./result/single/$input.txt!{$clear}\n";
+	}
+}
+
+function multipleList() {
+	include 'warna.php';
+
+	echo "Masukkan list: ";
+	$input = trim(fgets(STDIN));
+	$site = explode(PHP_EOL, file_get_contents($input));
+
+	if (!file_exists($input)) {
+		die("\n{$merah}File tidak ada!{$clear}\n");
+	} else {
+		foreach ($site as $no => $target) {
+			$ch = curl_init(); 
+			curl_setopt($ch, CURLOPT_URL, "https://sonar.omnisint.io/subdomains/$target");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+			$output = curl_exec($ch); 
+			curl_close($ch);
+
+			$hasil = json_decode($output, true);
+			echo "$clear\n";
+
+			if (is_null($hasil)) {
+				echo "{$merah}$target : site doesn't have subdomain!{$clear}\n";
+			} else {
+				echo "{$clear}" . ++$no . ". $target ==> ".count($hasil)." subdomains {$cyan}(Saved in ./result/multiple/$target.txt!){$clear}";
+
+				foreach ($hasil as $list) {
+				    $fp = fopen("./result/multiple/$target.txt", 'a+');
+					fwrite($fp, "$list\n");
+					fclose($fp);
+				}
+			}
+		}
+
+		echo "\n";
+	}
+}
+
+banner();
---
+++ warna.php
@@ -0,0 +1,7 @@
+<?php
+
+$clear = "\e[0m";
+$merah = "\e[31m";
+$hijau = "\e[32m";
+$kuning = "\e[33m";
+$cyan = "\e[36m";
